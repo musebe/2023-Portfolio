03d67dc75d46a1d12f784ec1dfa3e76cbe160c0f
Eugene Musebe <eugenemusebe@Eugenes-MacBook-Pro-2.local>
2023-01-25 04:08:11 +0300

    implemented a function to get files by names

---
+++ lib/md.ts
@@ -0,0 +1,13 @@
+import { join } from "path";
+import fs from "fs";
+
+const getDir = (path: string) => join(process.cwd(), path);
+
+const getFileNames = (dir: string): string[] => {
+  return fs.readdirSync(dir);
+}
+
+export {
+  getFileNames,
+  getDir
+}
